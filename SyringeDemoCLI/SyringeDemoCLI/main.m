379a31a753ae0f182b75728382a92863c1afd0bf
Tomek <tomek.cejner@gmail.com>
2012-01-20 23:39:58 +0100

    initial commit

---
+++ SyringeDemoCLI/SyringeDemoCLI/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SyringeDemoCLI
+//
+//  Created by Tomek Cejner on 20.01.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
